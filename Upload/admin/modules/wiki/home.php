e51ca43035a5f880a82c933c8b7340ca36fc77b7
Krytic <krytical.programmer@gmail.com>
2016-02-07 18:28:36 +1300

    Adds in update checking

--- Upload/admin/modules/wiki/home.php
+++ Upload/admin/modules/wiki/home.php
@@ -54,14 +54,36 @@ if (!$mybb->input['action'])
 }
 else if ($mybb->input['action'] == "version_check")
 {
+	$updateurl = 'https://api.github.com/repos/Krytic/MyBBWiki/releases/latest';
+
 	$page->output_nav_tabs($sub_tabs, 'wiki');
 
 	$table = new Table;
 
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Not ideal in the least but we're not passing sensitive data. See: http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/
+	curl_setopt($ch, CURLOPT_USERAGENT, 'MyBBWiki by Krytic Update Checker. https://github.com/Krytic');
+	curl_setopt($ch, CURLOPT_URL, $updateurl);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($ch);
+	curl_close($ch);
+
+	$latest_release = json_decode($data, true);
+
+	$latest_release['tag_name2'] = str_replace('v', '', $latest_release['tag_name']); // releases are formatted v1.0.0 or v1.5.7
+
+	if(version_compare(WIKI_VERSION, $latest_release['tag_name2']) < 0 && !$latest_release['prerelease']) {
+		$table->construct_header($lang->sprintf($lang->wiki_new_version, $latest_release['tag_name']), array("colspan" => 2));
+		$table->construct_row();
+
+		$table->construct_cell($latest_release['body']);
+		$table->construct_cell("<a href=\"{$latest_release['html_url']}\">" . $lang->new_release_download . "</a>", array('class' => 'align_center'));
+		$table->construct_row();
+		$table->output($lang->wiki_updates_header);
+	}
+
 	$table->construct_cell($lang->wiki_updates, array("class" => "align_center"));
 	$table->construct_row();
-
-	$table->output($lang->wiki_updates_head);
 }
 
 $page->output_footer();
