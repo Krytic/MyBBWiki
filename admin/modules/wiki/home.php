1fbd3557c5a06ab59a78f369b57b71255ca65636
Sean Richards <Krytic@users.noreply.github.com>
2025-05-05 17:58:27 +1200

    remove error reporting I missed

--- admin/modules/wiki/home.php
+++ admin/modules/wiki/home.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 // Disallow direct access to this file for security reasons
 if(!defined("IN_MYBB"))
 {
